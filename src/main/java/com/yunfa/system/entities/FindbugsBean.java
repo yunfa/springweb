08a0131237c7a3c9486b2cc644c96e682e331573
yunfa <195964032@qq.com>
2017-03-29 17:16:50 +0800

    add bean

---
+++ src/main/java/com/yunfa/system/entities/FindbugsBean.java
@@ -0,0 +1,14 @@
+package com.yunfa.system.entities;
+
+public class FindbugsBean {
+
+	private java.util.Date firstdate;
+
+	public void setFirstdate(java.util.Date value) {
+		this.firstdate = value;
+	}
+
+	public java.util.Date getFirstdate() {
+		return this.firstdate;
+	}
+}
